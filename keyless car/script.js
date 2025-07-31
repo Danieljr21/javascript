5a8738f43376e45764638711de0dfd96313b6946
Danieljr21 <emmanuelladaniel737@gmail.com>
2025-07-31 13:24:29 -0700

    Add files via upload

---
+++ keyless car/script.js
@@ -0,0 +1,13 @@
+console.log("JavaScript is linked!");
+alert("Welcome to my site!");
+var age = prompt("What is your age?");
+
+if (age < 18) {
+    alert("Sorry, you are too young to drive this car. Powering off");
+} else if (age == 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+} else {
+    alert("Powering On. Enjoy the ride!");
+}
+
+
